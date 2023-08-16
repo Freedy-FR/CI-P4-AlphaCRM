de4680d041a855d7d82610f61277801c0d42328a
Freedy-FR <>
2023-08-16 22:00:15 +0000

    feat: :sparkles: Add decorator and class to admin customer class

--- crm/admin.py
+++ crm/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Customer
 
-admin.site.register(Customer)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'company', 'job_title',
+                    'created_on', 'updated_on')
+    search_fields = ['full_name', 'content', 'company']
+    list_filter = ('company', 'created_on', 'updated_on')
