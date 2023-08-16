6f45ca1a638acf25528a73b4d5c964aa2492afd2
Freedy-FR <>
2023-08-16 21:11:45 +0000

    feat: :sparkles: Add static files from startbootstrap templates

--- crm/views.py
+++ crm/views.py
@@ -5,6 +5,6 @@ from .models import Customer
 
 class CustomerList(generic.ListView):
     model = Customer
-    queryset = Customer.objects.all()
+    queryset = Customer.objects.all().order_by("-created_on")
     template_name = "index.html"
     paginate_by = 20
