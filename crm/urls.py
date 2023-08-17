68254a26b79116dad9b5f058d8d023f1227ab7a9
Freedy-FR <>
2023-08-17 23:55:55 +0000

    refactor: :sparkles: Add login_required decoratoros to view and url

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,11 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
-    path('', views.CustomerList.as_view(), name="home"),
+    path('', views.RedirectIndex, name='index'),
+    # path('accounts/login/', views.RedirectLogin, name='login'),
+    path('customer_list/', login_required(views.CustomerList.as_view()), name='customer'),
+
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Customer
+from django.contrib.auth.decorators import login_required
 
 
 class CustomerList(generic.ListView):
-    model = Customer
-    queryset = Customer.objects.all().order_by('full_name')
-    template_name = "index.html"
-    paginate_by = 20
+    def get(self, request, *args, **kwargs):
+        queryset = Customer.objects.all().order_by('full_name')
+        model = Customer
+        template_name = "customer_list.html"
+        paginate_by = 20
+        return render(request, 'customer_list.html', {'queryset': queryset})
+
+
+@login_required
+def RedirectIndex(request):
+    return render(request, 'index.html', {})
+
+
+# def RedirectIndex(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'index.html', {})
+#     else:
+#         response = redirect('login')
+#         return response
+
+
+# def RedirectLogin(request):
+#     response = redirect('/accounts/login/')
+#     return response
+
+# response = redirect('index')
+# return response
+
+# class CustomerList(generic.ListView):
+#     def get(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             queryset = Customer.objects.all().order_by('full_name')
+#             model = Customer
+#             template_name = "customer_list.html"
+#             paginate_by = 20
+#             return render(request, 'customer_list.html', {'queryset': queryset})
+#         else:
+#             return redirect('login')
