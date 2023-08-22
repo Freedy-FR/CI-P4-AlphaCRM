674349a46e6aa5c6f4ddba77735faf8551385948
Freedy-FR <freedy.ramos@hotmail.com>
2023-08-22 03:41:12 +0000

    feat: :sparkles: Add javascript function to make the tr on customer list clickable

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,33 @@
-
-
 // Code source: Startbootstrap templates
 window.addEventListener("DOMContentLoaded", (event) => {
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector("#sidebarToggle");
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-            document.body.classList.toggle('sb-sidenav-toggled');
-        }
-        sidebarToggle.addEventListener("click", (event) => {
-            event.preventDefault();
-            document.body.classList.toggle("sb-sidenav-toggled");
-            localStorage.setItem(
-                "sb|sidebar-toggle",
-                document.body.classList.contains("sb-sidenav-toggled")
-            );
-        });
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector("#sidebarToggle");
+  if (sidebarToggle) {
+    // Uncomment Below to persist sidebar toggle between refreshes
+    if (localStorage.getItem("sb|sidebar-toggle") === "true") {
+      document.body.classList.toggle("sb-sidenav-toggled");
     }
+    sidebarToggle.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.toggle("sb-sidenav-toggled");
+      localStorage.setItem(
+        "sb|sidebar-toggle",
+        document.body.classList.contains("sb-sidenav-toggled")
+      );
+    });
+  }
+});
+
+// Custom Javascript
+
+document.addEventListener("DOMContentLoaded", function () {
+  var clickableRows = document.querySelectorAll(".clickable-row");
+  clickableRows.forEach(function (row) {
+    row.addEventListener("click", function () {
+      var href = row.getAttribute("data-href");
+      if (href) {
+        window.location.href = href;
+      }
+    });
+  });
 });
