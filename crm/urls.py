e240c444e63bfd41576510f886dbd94768115dfd
Freedy-FR <freedy.ramos@hotmail.com>
2023-08-22 22:55:17 +0000

    refactor: :art: Change the function views to class based views

--- crm/urls.py
+++ crm/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', views.RedirectIndex, name='index'),
-    path('instruction/', login_required(views.InstructionPage), name='instruction'),
+    path('', login_required(views.RedirectIndexView.as_view()), name='index'),
+    path('instruction/', login_required(views.InstructionPageView.as_view()), name='instruction'),
     path('add_customer/', login_required(views.AddCustomerView.as_view()), name='add_customer'),
     path('customer_list/', login_required(views.CustomerList.as_view()), name='customer_list'),
     path('customer_detail/<int:pk>/', login_required(views.CustomerDetailView.as_view()), name='customer_detail'),
--- crm/views.py
+++ crm/views.py
@@ -86,40 +86,11 @@ class DeleteCustomerView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('customer_list')
 
 
+class RedirectIndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', {})
 
 
-@login_required
-def RedirectIndex(request):
-    return render(request, 'index.html', {})
-
-
-@login_required
-def InstructionPage(request):
-    return render(request, 'instruction.html', {})
-
-
-# def RedirectIndex(request):
-#     if request.user.is_authenticated:
-#         return render(request, 'index.html', {})
-#     else:
-#         response = redirect('login')
-#         return response
-
-
-# def RedirectLogin(request):
-#     response = redirect('/accounts/login/')
-#     return response
-
-# response = redirect('index')
-# return response
-
-# class CustomerList(generic.ListView):
-#     def get(self, request, *args, **kwargs):
-#         if request.user.is_authenticated:
-#             queryset = Customer.objects.all().order_by('full_name')
-#             model = Customer
-#             template_name = "customer_list.html"
-#             paginate_by = 20
-#             return render(request, 'customer_list.html', {'queryset': queryset})
-#         else:
-#             return redirect('login')
+class InstructionPageView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'instruction.html', {})
