e05e899db538d17ff2835d54dcf76368be5dbe62
Freedy-FR <freedy.ramos@hotmail.com>
2023-08-24 01:38:17 +0000

    fix: :rocket: Change debug to false to test heroku app

--- alphaproject/settings.py
+++ alphaproject/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'alpha-crm-c833e9f43a21.herokuapp.com',
